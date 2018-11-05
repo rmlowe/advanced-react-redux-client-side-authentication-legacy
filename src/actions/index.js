b21c8106e9fd5bd4bdeea2555baa5a0c5461716f
Robert Lowe <robertmlowe@rmlowe.com>
2018-11-06 00:57:44 +0800

    LocalStorage and JWT

--- src/actions/index.js
+++ src/actions/index.js
@@ -12,6 +12,7 @@ export function signinUser({ email, password }, callback) {
         // - Update state to indicate user is authenticated
         dispatch({ type: AUTH_USER });
         // - Save the JWT token
+        localStorage.setItem('token', response.data.token);
         // - redirect to the route '/feature'
         callback();
       })
