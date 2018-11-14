da226814e0963b6bbcc8f73f72fec451a4429e98
Robert Lowe <robertmlowe@rmlowe.com>
2018-11-14 13:42:08 +0800

    Signup Component

--- src/components/app.js
+++ src/components/app.js
@@ -5,6 +5,7 @@ import { Route } from 'react-router-dom';
 import Header from './header';
 import Signin from './auth/signin';
 import Signout from './auth/signout';
+import Signup from './auth/signup';
 
 export default class App extends Component {
   render() {
@@ -13,6 +14,7 @@ export default class App extends Component {
         <Header />
         <Route path="/signin" component={Signin} />
         <Route path="/signout" component={Signout} />
+        <Route path="/signup" component={Signup} />
       </div>
     );
   }
