4c366f46fee5aca4b38042c032bdb356afa48d31
Robert Lowe <robertmlowe@rmlowe.com>
2018-11-14 18:30:32 +0800

    Securing Individual Routes

--- src/components/app.js
+++ src/components/app.js
@@ -6,6 +6,8 @@ import Header from './header';
 import Signin from './auth/signin';
 import Signout from './auth/signout';
 import Signup from './auth/signup';
+import Feature from './feature';
+import RequireAuth from './auth/require_auth';
 
 export default class App extends Component {
   render() {
@@ -15,6 +17,7 @@ export default class App extends Component {
         <Route path="/signin" component={Signin} />
         <Route path="/signout" component={Signout} />
         <Route path="/signup" component={Signup} />
+        <Route path="/feature" component={RequireAuth(Feature)} />
       </div>
     );
   }
