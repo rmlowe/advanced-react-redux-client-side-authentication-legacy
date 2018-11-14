f2309761fa4ced1eab65d74eb2f3f8f76d79bfe8
Robert Lowe <robertmlowe@rmlowe.com>
2018-11-14 15:01:02 +0800

    Implementing Validation Logic

--- src/components/auth/signup.js
+++ src/components/auth/signup.js
@@ -2,31 +2,41 @@ import React, { Component } from 'react';
 import { reduxForm, Field } from 'redux-form';
 import * as actions from '../../actions';
 
+const renderField = ({ input, label, type, meta: { touched, error } }) => (
+  <fieldset className="form-group">
+    <label>{label}:</label>
+    <input {...input} type={type} className="form-control" />
+    {touched && error && <div className="error">{error}</div>}
+  </fieldset>
+)
+
 class Signup extends Component {
   render() {
     const { handleSubmit, fields: { email, password, passwordConfirm }} = this.props;
 
     return (
       <form>
-        <fieldset className="form-group">
-          <label>Email:</label>
-          <Field name="email" type="text" component="input" autoComplete="none" className="form-control" />
-        </fieldset>
-        <fieldset className="form-group">
-          <label>Password:</label>
-          <Field name="password" type="password" component="input" autoComplete="none" className="form-control" />
-        </fieldset>
-        <fieldset className="form-group">
-          <label>Confirm Password:</label>
-          <Field name="passwordConfirm" type="password" component="input" autoComplete="none" className="form-control" />
-        </fieldset>
+        <Field name="email" type="text" component={renderField} label="Email" />
+        <Field name="password" type="password" component={renderField} label="Password" />
+        <Field name="passwordConfirm" type="password" component={renderField} label="Confirm Password" />
         <button action="submit" className="btn btn-primary">Sign up!</button>
       </form>
     );
   }
 }
 
+function validate(formProps) {
+  const errors = {};
+
+  if (formProps.password !== formProps.passwordConfirm) {
+    errors.password = 'Passwords must match';
+  }
+
+  return errors;
+}
+
 export default reduxForm({
   form: 'signup',
-  fields: ['email', 'password', 'passwordConfirm']
+  fields: ['email', 'password', 'passwordConfirm'],
+  validate
 })(Signup);
