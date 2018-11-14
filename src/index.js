10cbb5521dbea86eadbccc491c8268ec76edec44
Robert Lowe <robertmlowe@rmlowe.com>
2018-11-14 18:50:00 +0800

    Automatically Authenticating Users

--- src/index.js
+++ src/index.js
@@ -7,11 +7,20 @@ import reduxThunk from 'redux-thunk';
 
 import App from './components/app';
 import reducers from './reducers';
+import { AUTH_USER } from './actions/types';
 
 const createStoreWithMiddleware = applyMiddleware(reduxThunk)(createStore);
+const store = createStoreWithMiddleware(reducers);
+
+const token = localStorage.getItem('token');
+// If we have a token, consider the user to be signed in
+if (token) {
+  // we need to update application state
+  store.dispatch({ type: AUTH_USER });
+}
 
 ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
+  <Provider store={store}>
     <BrowserRouter>
       <Route path="/" component={App} />
     </BrowserRouter>
